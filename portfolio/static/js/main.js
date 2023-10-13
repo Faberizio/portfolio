4c15575be88fb5fc5bb3cbc1eaedfc2d8100bf74
Fabrizio Golino <124782920+Faberizio@users.noreply.github.com>
2023-10-13 16:23:42 +0200

    Delete portfolio/static directory

--- portfolio/static/js/main.js
+++
@@ -1,9 +0,0 @@
-function changeTheme(theme) {
-    if (theme == 'dark') {
-        document.body.style.backgroundColor = 'black';
-        document.body.style.color = 'white';
-    } else {
-        document.body.style.backgroundColor = '';
-        document.body.style.color = '';
-    }
-}
